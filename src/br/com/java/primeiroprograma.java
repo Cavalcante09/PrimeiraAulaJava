c4bc3ee563777a1ce72ecc5a766e25b4f0082843
Alisson Cavalcante <alissonsilva0255@gmail.com>
2023-03-30 23:31:39 -0300

    First Commit

---
+++ src/br/com/java/primeiroprograma.java
@@ -0,0 +1,17 @@
+package br.com.java;
+
+import br.com.java.model.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		
+			Gato gato = new Gato ();
+			System.out.println(gato);
+		
+		/*int a = 1;
+			int b = 2;
+			System.out.println("Hello, World! " + (a+b));*/
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author proxx
+ *
+ */
+module PrimeiraAulaJava {
+}
